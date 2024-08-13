f8c1cbbcaa361c6157ddc6c1821e486332c0f72f
moktadir-mirul <moktadir.mirul@gmail.com>
2024-08-13 06:32:15 +0600

    Products fetching from shop is done by async function, on time revalidation done.

---
+++ src/app/Components/Nav.jsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+
+export const Nav= () => {
+    return(
+        <div>
+            <ul id="ulID1">
+                <li><Link href={"/"}>Home</Link></li>
+                <li><Link href={"/Shop"}>Shop</Link></li>
+                <li><Link href={"/AllNotes"}>All Notes</Link></li>
+                <li><Link href={"/AddProductFrom"}>Add Product</Link></li>
+                <li><Link href={"/Posts"}>Posts</Link></li>
+            </ul>
+        </div>
+    )
+}
